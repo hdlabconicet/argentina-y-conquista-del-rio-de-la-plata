84e5877d6ae68191703a250c9ad19581e6ec49df
nidiah <nidinhernandez@gmail.com>
2021-04-08 12:52:47 -0300

    new layout proposal

--- assets/js/map-balloon.js
+++ assets/js/map-balloon.js
@@ -80,7 +80,7 @@
 
         loadPreview: function() {       
             var lugar = this.$el.text().trim();
-            var coordenadas = acaretePlaces[lugar] // acaretePlaces esta definido en acarete-places.js    
+            var coordenadas = centeneraPlaces[lugar] // 
             var map = L.map('map'+this.counter).setView(coordenadas, 3);
             L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
