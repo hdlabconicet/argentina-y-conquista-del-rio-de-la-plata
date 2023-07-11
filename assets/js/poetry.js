39ee3e2fffed6153121df8f026e9fb4f45bde409
nidiah <nidinhernandez@gmail.com>
2023-07-11 19:17:56 -0300

    show gloss sample

--- assets/js/poetry.js
+++ assets/js/poetry.js
@@ -1,7 +1,9 @@
 const rhymeBtn = document.getElementById("show_rhyme")
 const scansionBtn = document.getElementById("show_scansion")
+const glossBtn = document.getElementById("show_gloss")
 const scandedStanzas = document.querySelector("div.scansion")
 const rhymeStanzas = document.querySelector("div.rhyme")
+const glossStanzas = document.querySelector("div.gloss")
 
 
 function markRhyme(){
@@ -10,6 +12,7 @@ function markRhyme(){
 	rhymeBtn.addEventListener("click", (evt)=> {
 		rhymeStanzas.style.display = "block"
 		scandedStanzas.style.display = "none"
+		glossStanzas.style.display = "none"
 		for (let rhyme of rhymes) {
 			rhyme.classList.toggle("active_rhyme")
 		}
@@ -19,8 +22,16 @@ function showScansion(){
 	scansionBtn.addEventListener("click", (evt)=> {
 		scandedStanzas.style.display = "block"
 		rhymeStanzas.style.display = "none"
+		glossStanzas.style.display = "none"
 })}
 
+function showGloss(){
+	glossBtn.addEventListener("click", (evt)=> {
+		glossStanzas.style.display = "block"
+		rhymeStanzas.style.display = "none"
+		scandedStanzas.style.display = "none"
+})}
 
 markRhyme()
 showScansion()
+showGloss()
